e5612baee1c8fadbbc000cd8ac69bc917e4afc78
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-10-25 21:26:49 -0300

    feat: vercel routes config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
-// import Register from './pages/Register';
 import { Dashboard } from "./components/Dashboard";
 import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
